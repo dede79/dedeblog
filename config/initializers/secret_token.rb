913d845ffa7b0812347e97070089ce546e38042e
andreahurren <limahawkins@gmail.com>
2015-08-14 19:53:41 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dedeblog::Application.config.secret_token = 'baab86457353c3f1c8b5d68bc32ed97bb03dcebb1be73ac93f34fd02107bebd7873a0f746070b778f78111227aad28221257c5f3da8da9c7958b9bdb530f87b8'
